b52e21950c6d3264bd8ed0a919da313211e38bf8
Meysam Rahdari <meysamra7@gmail.com>
2025-03-29 19:31:27 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from aiogram import Bot, Dispatcher, executor
-  import os
+import os
 
-  TOKEN = os.getenv("TOKEN")  # توکن از محیط اجرا گرفته می‌شود
-  bot = Bot(token=TOKEN)
-  dp = Dispatcher(bot)
+TOKEN = os.getenv("TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-  @dp.message_handler(commands=['start'])
-  async def start(message):
-      await message.reply("ربات شما با موفقیت فعال شد! 🤖")
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.reply("ربات شما با موفقیت فعال شد! 🤖")
 
-  if __name__ == '__main__':
-      executor.start_polling(dp)
+if __name__ == '__main__':
+    executor.start_polling(dp)
