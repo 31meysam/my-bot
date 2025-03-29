a0993b9baa5f16cf5f34fe37eae1a5e4c2be2af8
Meysam Rahdari <meysamra7@gmail.com>
2025-03-29 19:56:35 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 import os
+
 TOKEN = os.getenv("TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
 @dp.message_handler(commands=['start'])
 async def start(message):
     await message.reply("ربات شما با موفقیت فعال شد! 🤖")
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
